5f0def55503785129b795fb7aabcdaf34ee39154
Steven Ke <spp72kimo@gmail.com>
2022-02-21 22:11:59 +0800

    '新增氣象查詢'

--- main.py
+++ main.py
@@ -113,8 +113,8 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    # app.run()
-    port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+    # port = int(os.environ.get('PORT', 80))
+    # app.run(host='0.0.0.0', port=port)
 
     
